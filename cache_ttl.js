a9958461dc89736cd1d1841e58bc2de3b8ab08b1
vidy alfredo <vidy.traveloka@gmail.com>
2017-10-09 14:28:46 +0700

    remove all the test from main file

--- cache_ttl.js
+++ cache_ttl.js
@@ -109,26 +109,4 @@ function deleteTTL(obj, string_path, ttl){
     }, ttl) 
 }
 
-
-const cache = Cache({});
-
-const Project = {
-  name_project: 'bathroom',
-  images: ['http://asdfsafd', 'http:asdfasfd']
-};
-
-cache.put_cache("test", Project, 1000);
-cache.put_cache("test.hello", "world", 1000);
-cache.put_cache("test.hi", "world", 5000);
-cache.delete_cache('test.hello')
-cache.get_cache_promise("test.hi")
- .then(console.log)
- .catch(err => {
-
- })
-
-setTimeout(() => cache.get_cache_promise("test.hi")
- .then(console.log)
- .catch(err=> {
-     
- }), 1100);
+module.exports = Cache;
